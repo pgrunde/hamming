ad87e456adc3c251e33238b87f6309025ad01a8a
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-18 14:35:49 -0600

    Solo redo with 12 lines of code

--- hamming.rb
+++ hamming.rb
@@ -2,25 +2,18 @@ require_relative "testing_library"
 
 def hamming(strand_1, strand_2)
   hamming = 0
-  arr_1 = strand_1.split(//)
-  arr_2 = strand_2.split(//)
-
-  if arr_1.length < arr_2.length
-    shortest_length = arr_1.length
-  else
-    shortest_length = arr_2.length
+  arr1 = strand_1.split(//)
+  arr2 = strand_2.split(//)
+  while true
+    compare_1 = arr1.shift
+    compare_2 = arr2.shift
+    break if compare_1 == nil || compare_2 == nil
+    hamming += 1 if compare_1 != compare_2
   end
-
-  iter = 0
-  shortest_length.times do
-    hamming += 1 if arr_1[iter] != arr_2[iter]
-    iter += 1
-  end
-
   hamming
 end
 
-
+p hamming("AAGCTA","AAGCTC")
 
 check("Hamming distance between identical strands",
       hamming("A", "A") == 0)
