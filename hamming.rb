00d0b60bf653e89f77d3919c58f3dfea2e84fd7e
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-18 14:22:41 -0600

    Hamming like a boss

--- hamming.rb
+++ hamming.rb
@@ -1,8 +1,26 @@
 require_relative "testing_library"
 
 def hamming(strand_1, strand_2)
+  hamming = 0
+  arr_1 = strand_1.split(//)
+  arr_2 = strand_2.split(//)
 
+  if arr_1.length < arr_2.length
+    shortest_length = arr_1.length
+  else
+    shortest_length = arr_2.length
+  end
+
+  iter = 0
+  shortest_length.times do
+    hamming += 1 if arr_1[iter] != arr_2[iter]
+    iter += 1
+  end
+
+  hamming
 end
+
+
 
 check("Hamming distance between identical strands",
       hamming("A", "A") == 0)
